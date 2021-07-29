51981cb0753e682b71f809c8aec08ce8ee1d337a
Akahibe Kingsley <divine10646@gmail.com>
2021-07-29 18:00:09 +0100

    created the header and about section

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("header-menu").style.width = "100%";
+}
+
+function closeMenu() {
+    document.getElementById("header-menu").style.width = "0%";
+}
