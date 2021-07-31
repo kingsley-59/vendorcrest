d9dfb1216992842bcb9e8cfaceefe4e9de948ce4
Akahibe Kingsley <divine10646@gmail.com>
2021-07-31 13:46:28 +0100

    Added more sections to the home page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 function openMenu() {
+    document.getElementById("menu-overlay").style.display = "block";
     document.getElementById("header-menu").style.width = "100%";
 }
 
 function closeMenu() {
+    document.getElementById("menu-overlay").style.display = "none";
     document.getElementById("header-menu").style.width = "0%";
 }
